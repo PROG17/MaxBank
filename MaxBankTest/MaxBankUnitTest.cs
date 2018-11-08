3547a937e9bd310897b5b3662b6f422bbd077190
Max Mark <maxmark92@gmail.com>
2018-11-08 14:16:04 +0100

    Added tests

---
+++ MaxBankTest/MaxBankUnitTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace MaxBankTest
+{
+    public class MaxBankUnitTest
+    {
+        [Fact]
+        public void Withdraw()
+        {
+
+        }
+
+        [Fact]
+        public void Deposit()
+        {
+
+        }
+
+        [Fact]
+        public void Transfer()
+        {
+
+        }
+    }
+}
